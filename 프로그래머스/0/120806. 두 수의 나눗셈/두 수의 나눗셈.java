cb2c3319c5609bc90115a181dbaee91007f5bcf4
rang <withghd@gmail.com>
2025-02-28 23:34:20 +0900

    두 수의 나눗셈

--- 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -1,8 +1,7 @@
 class Solution {
     public int solution(int num1, int num2) {
         double div = (double) num1 / num2 * 1000;
-        int answer = (int) div; 
-        
+        int answer = (int) div;
         return answer;
     }
 }
