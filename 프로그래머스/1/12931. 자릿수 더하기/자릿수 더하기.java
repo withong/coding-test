aa47f965d8f11bda25faeadb858744a577cac46d
rang <withghd@gmail.com>
2025-03-02 17:27:24 +0900

    자릿수 더하기

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -1,6 +1,7 @@
 import java.util.*;
 
 public class Solution {
+
     public int solution(int num) {
         int answer = Arrays.stream(String.valueOf(num).split(""))
             .mapToInt(Integer::parseInt)
