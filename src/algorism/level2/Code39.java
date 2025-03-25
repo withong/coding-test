979362af06953de59840d88554c46938b7db5eec
rang <withghd@gmail.com>
2025-03-25 20:14:22 +0900

    최대공약수와 최소공배수

---
+++ src/algorism/level2/Code39.java
@@ -0,0 +1,24 @@
+package algorism.level2;
+
+import java.math.BigInteger;
+
+public class Code39 {
+/*
+    두 수를 입력받아 두 수의 최대공약수와 최소공배수를 반환하는 함수, solution을 완성해 보세요.
+    배열의 맨 앞에 최대공약수, 그다음 최소공배수를 넣어 반환하면 됩니다.
+    예를 들어 두 수 3, 12의 최대공약수는 3, 최소공배수는 12이므로 solution(3, 12)는 [3, 12]를 반환해야 합니다.
+*/
+    public static void main(String[] args) {
+
+    }
+
+    public static int[] solution(int n, int m) {
+        BigInteger a = BigInteger.valueOf(n);
+        BigInteger b = BigInteger.valueOf(m);
+
+        int gcd = a.gcd(b).intValue();
+        int lcm = n * m / gcd;
+
+        return new int[]{gcd, lcm};
+    }
+}
