bb08a54cf7a451add57bdc6896c4a5300cf36f77
rang <withghd@gmail.com>
2025-03-25 09:55:47 +0900

    입양 시각 구하기(1)

--- 프로그래머스/2/59412. 입양 시각 구하기（1）/입양 시각 구하기（1）.sql
+++ 프로그래머스/2/59412. 입양 시각 구하기（1）/입양 시각 구하기（1）.sql
@@ -1,5 +1,5 @@
-select date_format(DATETIME, "%H") as HOUR
-    ,count(*) as COUNT
+select date_format(DATETIME, "%H") as HOUR,
+       count(*) as COUNT
 from ANIMAL_OUTS 
 where TIME(DATETIME) between "09:00" and "19:59"
 group by HOUR
