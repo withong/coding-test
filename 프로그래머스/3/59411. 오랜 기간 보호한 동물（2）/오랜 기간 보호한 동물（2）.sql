a518646cc02130e571d318f1427ac22698ffbffe
rang <withghd@gmail.com>
2025-04-03 10:29:09 +0900

    오랜 기간 보호한 동물(2)

--- 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
+++ 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
@@ -1,5 +1,4 @@
 select i.ANIMAL_ID, i.NAME
 from ANIMAL_INS i
 join ANIMAL_OUTS o on i.ANIMAL_ID = o.ANIMAL_ID
-order by datediff(o.DATETIME, i.DATETIME) desc
-limit 2
+order by datediff(o.DATETIME, i.DATETIME) desc limit 2
