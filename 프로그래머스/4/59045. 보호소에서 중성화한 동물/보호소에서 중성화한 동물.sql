bf7a8509f68b1c972dc3c4a95221df1d17f98f4f
rang <withghd@gmail.com>
2025-04-04 18:29:21 +0900

    보호소에서 중성화한 동물

--- 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -2,5 +2,5 @@ select i.ANIMAL_ID, i.ANIMAL_TYPE, i.NAME
 from ANIMAL_INS i
 join ANIMAL_OUTS o on i.ANIMAL_ID = o.ANIMAL_ID
 where i.SEX_UPON_INTAKE like 'Intact%' 
-and o.SEX_UPON_OUTCOME not like 'Intact%'
+    and o.SEX_UPON_OUTCOME not like 'Intact%'
 order by i.ANIMAL_ID
