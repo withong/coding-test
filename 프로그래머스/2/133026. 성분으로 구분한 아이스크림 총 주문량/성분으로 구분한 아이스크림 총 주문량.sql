c0a56a344fabb68872e41543795f8bdcf8ba0350
rang <withghd@gmail.com>
2025-04-14 09:55:36 +0900

    성분으로 구분한 아이스크림 총 주문량

--- 프로그래머스/2/133026. 성분으로 구분한 아이스크림 총 주문량/성분으로 구분한 아이스크림 총 주문량.sql
+++ 프로그래머스/2/133026. 성분으로 구분한 아이스크림 총 주문량/성분으로 구분한 아이스크림 총 주문량.sql
@@ -2,4 +2,4 @@ select i.INGREDIENT_TYPE, sum(f.TOTAL_ORDER) as TOTAL_ORDER
 from FIRST_HALF f
 join ICECREAM_INFO i on f.FLAVOR = i.FLAVOR
 group by i.INGREDIENT_TYPE
-order by f.TOTAL_ORDER;
+order by f.TOTAL_ORDER
