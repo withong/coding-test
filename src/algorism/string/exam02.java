224426ca5801c0150960e074d20c8ccd644e7bd8
rang <withghd@gmail.com>
2025-05-29 23:34:57 +0900

    대소문자 변환

---
+++ src/algorism/string/exam02.java
@@ -0,0 +1,24 @@
+ package algorism.string;
+
+import java.util.Scanner;
+
+public class exam02 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String input = in.nextLine();
+
+        StringBuilder answer = new StringBuilder();
+
+        for (char c : input.toCharArray()) {
+            if (Character.isLowerCase(c)) {
+                c = Character.toUpperCase(c);
+            } else {
+                c = Character.toLowerCase(c);
+            }
+            answer.append(c);
+        }
+
+        System.out.println(answer);
+    }
+}
