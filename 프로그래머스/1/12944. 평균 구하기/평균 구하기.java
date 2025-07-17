fe1c36d6e787b868864e36d972d2b06e9e4e11d7
rang <withghd@gmail.com>
2025-07-18 00:04:58 +0900

    [level 1] Title: 평균 구하기, Time: 0.04 ms, Memory: 83.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public double solution(int[] arr) {
+        int arrSum = 0;
+        
+        for (int value : arr) {
+            arrSum += value;
+        }
+        
+        return (double) arrSum / arr.length;
+    }
+}
