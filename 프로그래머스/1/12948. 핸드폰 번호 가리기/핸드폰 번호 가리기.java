dd6986218b2a1f63f57ee872d7eaffbbc400e28b
rang <withghd@gmail.com>
2025-07-18 00:36:13 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.06 ms, Memory: 85.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String phone_number) {
+        int length = phone_number.length();
+        int setLength = length - 4;
+
+        StringBuilder stringBuilder = new StringBuilder(phone_number);
+
+        for (int i = 0; i < setLength; i++) {
+            stringBuilder.setCharAt(i, '*');
+        }
+
+        String answer = stringBuilder.toString();
+        return answer;
+    }
+}
