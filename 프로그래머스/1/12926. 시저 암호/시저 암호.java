00c28f1204a18978ccb006804281a752a614d1d0
rang <withghd@gmail.com>
2025-07-18 01:13:09 +0900

    [level 1] Title: 시저 암호, Time: 1.26 ms, Memory: 80.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String solution(String s, int n) {
+        StringBuilder answer = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (c == ' ') {
+                answer.append(' ');
+            } else if (c >= 'A' && c <= 'Z') {
+                int push = c + n;
+                if (push > 'Z') push -= 26;
+                answer.append((char) push);
+            } else if (c >= 'a' && c <= 'z') {
+                int push = c + n;
+                if (push > 'z') push -= 26;
+                answer.append((char) push);
+            }
+        }
+
+        return answer.toString();
+    }
+}
