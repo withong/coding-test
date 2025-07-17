e19e05d66c531118768eac540211c328159a0de9
rang <withghd@gmail.com>
2025-07-18 00:12:58 +0900

    [level 1] Title: 약수의 합, Time: 0.04 ms, Memory: 88.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int num) {
+        int answer = 0;
+        int sqrt = (int) Math.sqrt(num);
+
+        for (int i = 1; i <= sqrt; i++) {
+            if (num % i == 0) {
+                answer += i;
+                if (i != (num / i)) {
+                    answer += num/i;
+                }
+            }
+        }
+        return answer;
+    }
+}
