455e7ecc2045ad4e4335bc6d7d9bb46de8fe9045
rang <withghd@gmail.com>
2025-07-18 00:42:00 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 1.49 ms, Memory: 86 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(int n) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < n; i++) {
+            sb.append(i % 2 == 0 ? "수" : "박");
+        }
+        return sb.toString();        
+    }
+}
