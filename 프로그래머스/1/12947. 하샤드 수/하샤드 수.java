d2bd14d636462136d553ea3c5def3dc5fc326bd9
rang <withghd@gmail.com>
2025-07-18 00:24:04 +0900

    [level 1] Title: 하샤드 수, Time: 2.32 ms, Memory: 78.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean solution(int x) {
+        String str = String.valueOf(x);
+        int sum = str.chars().map(c -> c - '0').sum();
+
+        return x % sum == 0;
+    }
+}
