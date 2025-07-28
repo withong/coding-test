3dc1d4e9785521727f56db80e2612db70d1cd956
rang <withghd@gmail.com>
2025-07-28 18:58:11 +0900

    01-04 복습

--- 인프런/01/04. 단어 뒤집기/단어 뒤집기.java
+++ 인프런/01/04. 단어 뒤집기/단어 뒤집기.java
@@ -1,43 +1,43 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
 
-        int count = scanner.nextInt();
-        scanner.nextLine();
+    public String[] solution(int count, String[] arr) {
+        String[] answer = new String[count];
 
-        String[] strArr = new String[count];
-
-        for (int i = 0; i < strArr.length; i++) {
-            strArr[i] = scanner.nextLine();
-        }
-
-        String[] answer = solution2(count, strArr);
-
-        for (String s : answer) {
-            System.out.println(s);
-        }
-    }
-
-    public static String[] solution2(int num, String[] strArr) {
-        String[] answer = new String[num];
-
-        for (int i = 0; i < num; i++) {
-            char[] s = strArr[i].toCharArray();
+        for (int i = 0; i < count; i++) {
+            char[] chars = arr[i].toCharArray();
             int lt = 0;
-            int rt = strArr[i].length() - 1;
+            int rt = arr[i].length() - 1;
 
             while (lt < rt) {
-                char tmp = s[lt];
-                s[lt] = s[rt];
-                s[rt] = tmp;
+                char tmp = chars[lt];
+                chars[lt] = chars[rt];
+                chars[rt] = tmp;
 
                 lt++;
                 rt--;
             }
-            answer[i] = String.valueOf(s);
+            answer[i] = String.valueOf(chars);
         }
+
         return answer;
+    }
+
+    public static void main(String[] args) {
+        Main m = new Main();
+        Scanner in = new Scanner(System.in);
+
+        int count = in.nextInt();
+        in.nextLine();
+
+        String[] arr = new String[count];
+        for (int i = 0; i < count; i++) {
+            arr[i] = in.nextLine();
+        }
+
+        for (String answer : m.solution(count, arr)) {
+            System.out.println(answer);
+        }
     }
 }
