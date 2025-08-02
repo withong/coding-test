f21bb63c2426e022b8da060a9cdbf13b693618b0
rang <withghd@gmail.com>
2025-08-03 00:44:34 +0900

    02-04

---
+++ 인프런/02/04. 피보나치 수열/피보나치 수열.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public int[] solution(int n) {
+        int[] answer = new int[n];
+        answer[0] = 1;
+        answer[1] = 1;
+
+        for (int i = 2; i < n; i++) {
+            answer[i] = answer[i - 1] + answer[i - 2];
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main m = new Main();
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        for (int i : m.solution(n)) {
+            System.out.print(i + " ");
+        }
+    }
+}
