5d536a0d2ee97df66d9ca92edbf4173cd1b61d16
rang <withghd@gmail.com>
2025-08-08 18:50:25 +0900

    02-12

---
+++ 인프런/02/12. 멘토링/멘토링.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public int solution(int n, int m, int[][] arr) {
+        int answer = 0;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                int count = 0;
+
+                for (int k = 0; k < m; k++) {
+                    int pi = 0;
+                    int pj = 0;
+
+                    for (int s = 0; s < n; s++) {
+                        if (arr[k][s] == i) pi = s;
+                        if (arr[k][s] == j) pj = s;
+                    }
+                    if (pi < pj) count++;
+                }
+                if (count == m) answer++;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int m = in.nextInt();
+        int[][] arr = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = in.nextInt();
+            }
+        }
+
+        System.out.println(main.solution(n, m, arr));
+    }
+}
