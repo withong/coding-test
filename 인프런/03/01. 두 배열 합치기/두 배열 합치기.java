603371f16763a0df48865e0622fbb7073d63b010
rang <withghd@gmail.com>
2025-08-08 21:32:44 +0900

    03-01

---
+++ 인프런/03/01. 두 배열 합치기/두 배열 합치기.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    public ArrayList<Integer> solution(int n, int m, int[] a, int[] b) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        int p1 = 0;
+        int p2 = 0;
+
+        while (p1 < n && p2 < m) {
+            if (a[p1] < b[p2]) {
+                answer.add(a[p1++]);
+            } else {
+                answer.add(b[p2++]);
+            }
+        }
+
+        while (p1 < n) {
+            answer.add(a[p1++]);
+        }
+
+        while (p2 < m) {
+            answer.add(b[p2++]);
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = in.nextInt();
+        }
+
+        int m = in.nextInt();
+        int[] b = new int[m];
+        for (int i = 0; i < m; i++) {
+            b[i] = in.nextInt();
+        }
+
+        for (int i : main.solution(n, m, a, b)) {
+            System.out.print(i + " ");
+        }
+    }
+}
