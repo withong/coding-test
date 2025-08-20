70eb0531a1bfb112dc095f23b6ef9fb347f4ec37
rang <withghd@gmail.com>
2025-08-20 18:26:30 +0900

    05-01

---
+++ 인프런/05. Stack, Queue/01. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Main {
+
+    public String solution(String s) {
+        String answer = "YES";
+        Stack<Character> stack = new Stack<>();
+
+        for (char x : s.toCharArray()) {
+            if (x == '(') {
+                stack.push(x);
+            } else {
+                if (stack.isEmpty()) return "NO";
+                stack.pop();
+            }
+        }
+
+        if (!stack.isEmpty()) return "NO";
+        return answer;햣
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        Scanner in = new Scanner(System.in);
+
+        String s = in.next();
+        System.out.println(main.solution(s));
+    }
+}
