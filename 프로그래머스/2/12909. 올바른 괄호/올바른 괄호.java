4b82f88791264cf4361acf5cb9044916954c1b71
rang <withghd@gmail.com>
2025-09-02 19:46:49 +0900

    [level 2] Title: 올바른 괄호, Time: 21.29 ms, Memory: 56.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        Stack<Character> stack = new Stack<>();
+        
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) return false;
+                stack.pop();
+            }
+        }
+        
+        if (!stack.isEmpty()) return false;
+        return answer;
+    }
+}
