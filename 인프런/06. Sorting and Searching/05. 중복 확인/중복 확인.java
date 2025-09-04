5762277f5821d64a1376bb714d561290f28c1e6a
rang <withghd@gmail.com>
2025-09-04 14:32:11 +0900

    06-05

---
+++ 인프런/06. Sorting and Searching/05. 중복 확인/중복 확인.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Main {
+
+    public char solution(int n, int[] arr) {
+        char answer = 'U';
+
+        Arrays.sort(arr);
+
+        for (int i = 0; i < n - 1; i++) {
+            if (arr[i] == arr[i + 1]) return 'D';
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+
+        System.out.print(main.solution(n, arr));
+    }
+}
