c9bb318126437f605e21596633f0df0b69b2b8a6
rang <withghd@gmail.com>
2025-09-08 21:11:34 +0900

    06-08

---
+++ 인프런/06. Sorting and Searching/08. 이분검색/이분검색.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class Main {
+
+    public int solution(int n, int m, int[] arr) {
+        int answer = 0;
+        Arrays.sort(arr);
+
+        int lt = 0;
+        int rt = n - 1;
+
+        while (lt <= rt) {
+            int mid = (lt + rt) / 2;
+
+            if (arr[mid] == m) {
+                answer = mid + 1;
+                break;
+            }
+
+            if (arr[mid] > m) {
+                rt = mid - 1;
+            } else {
+                lt = mid + 1;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+        int m = in.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+
+        System.out.println(main.solution(n, m, arr));
+    }
+}
